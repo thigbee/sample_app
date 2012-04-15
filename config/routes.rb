d20ca564b5767a4c1e5550e7a898b70db0d5acae
thigbee <talanzzz@yahoo.com>
2012-04-15 11:24:36 -0400

    Completed 9.1 and 9.2.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 SampleApp::Application.routes.draw do
+  get "sessions/new"
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
 
   root :to => 'pages#home'
+
 
 # old statements from Chap 3 (tah)
   # get "pages/contact"
