3f7f3d04787a8780d53f7b8adc50533aaa758cef
thigbee <talanzzz@yahoo.com>
2012-04-15 18:05:06 -0400

    end of 10.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
 
   def show
@@ -40,6 +40,16 @@ class UsersController < ApplicationController
       @title = "Edit user"
       render 'edit'
     end
+  end
+  
+  def index
+    @title = "All users"
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
   end
   
   private
