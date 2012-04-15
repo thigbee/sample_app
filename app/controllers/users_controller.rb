c1e9a9d65b947412d8106aa0b5b93581e0c3d4c6
thigbee <talanzzz@yahoo.com>
2012-04-15 17:03:30 -0400

    after revising edit actions for authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+
   def show
    @user = User.find(params[:id])
    @title = @user.name
@@ -26,12 +29,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
     @title = "Edit user"
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated."
       redirect_to @user
@@ -40,4 +41,15 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  private
+  
+    def authenticate
+      deny_access unless signed_in?
+    end
+  
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
